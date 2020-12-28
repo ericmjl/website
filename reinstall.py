7341c2dd2ae0c68eaee6946aa356411b5bc54c9a
Eric Ma <ericmjl@users.noreply.github.com>
2020-12-27 19:05:41 -0500

    Removed a bunch of unnecessary config files (#33)

--- reinstall.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if __name__ == '__main__':
-    pkgname = sys.argv[1]
-
-    os.system('pip uninstall -y {0}'.format(pkgname))
-    os.system('conda install {0}'.format(pkgname))
-    os.system('conda env export > environment.yml')
--- update_env.sh
+++
@@ -1,2 +0,0 @@
-wget https://raw.githubusercontent.com/ericmjl/conda-envs/master/lektor.yml -O environment.yml
-conda env update -f environment.yml
