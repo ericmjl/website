eda18639c94d496eb80f10c8560b6c734c4d6fe0
Eric Ma <ericmajinglong@gmail.com>
2023-08-27 08:49:54 -0400

    feat: Refactor blogbot API and add search functionality
    This commit includes several changes to the blogbot API:
    - Import and use pathlib.Path for file path handling
    - Uncomment and refactor import statements for prompts and scraper
    - Refactor the home endpoint to use the get_latest_blog_posts function with a base_url parameter
    - Refactor the social_media endpoint to handle different types of social media and use the get_post_body function
    - Add new search_home endpoint for search functionality
    - Add new search endpoint to handle search requests
    - Add helper functions for blog text retrieval, publication date finding, and URL conversion
    
    This commit also removes several unused functions and updates the HTML templates used in the endpoints.

--- apis/blogbot/api.py
+++ apis/blogbot/api.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 from typing import Annotated
 
 import requests
@@ -8,12 +9,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from llamabot import SimpleBot
-
-# from llamabot.prompt_library.blog import (
-#     blogging_bot,
-#     compose_linkedin_post,
-#     compose_twitter_post,
-# )
+from .prompts import (
+    compose_linkedin_post,
+    compose_twitter_post,
+    compose_tags,
+    compose_summary,
+)
+from .scraper import get_latest_blog_posts, get_post_body
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="apis/blogbot/static"), name="static")
@@ -25,98 +27,106 @@ bot = SimpleBot(
 templates = Jinja2Templates(directory="apis/blogbot/templates")
 
 
-def compose_linkedin_post(text):
-    prompt = f"""
-This is a blog post that I just wrote.
-
-{text}
-
-Please compose for me a LinkedIn post
-that entices my network on LinkedIn to read it.
-Ensure that there is a call to action to interact with the post after reading
-to react with it, comment on it, or share the post with others,
-and to support my work on Patreon.
-My Patreon link is https://patreon.com/ericmjl/
-Include hashtags inline with the LinkedIn post and at the end of the post too.
-Please return this for me in JSON format using the following schema:
-
-
-    "post_text": "post text goes here"
-
-    """
-    return prompt
-
-
-def compose_twitter_post(text):
-    prompt = f"""
-This is a blog post that I just wrote:
-
-{text}
-
-Please compose for me a Twitter post
-that entices my followers on Twitter to read it.
-Ensure that there is a call to action to interact with the post after reading it,
-such as retweeting, commenting, or sharing it with others,
-and to support my work on Patreon.
-My Patreon link is https://patreon.com/ericmjl/
-Include hashtags inline with the Twitter post.
-
-Please return this for me in JSON format using the following schema:
-
-
-    "post_text": "post text goes here"
-
-    """
-    return prompt
-
-
-def get_latest_blog_posts() -> dict:
-    url = "https://ericmjl.github.io/blog/"
-    response = requests.get(url)
-    html = response.content
-    soup = BeautifulSoup(html, "html.parser")
-
-    # Find the <a> tags nested within the <header> tag
-    header = soup.find_all("header")
-    titles = [h.get_text().strip("\n") for h in header]
-    anchors = [h.find("a", href=True) for h in header]
-    hrefs = [url + a.get("href") for a in anchors]
-    # dictionary of link to title
-    links_to_titles = dict(zip(hrefs, titles))
-    return links_to_titles
-
-
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    latest_blog_posts = get_latest_blog_posts()
+    latest_blog_posts = get_latest_blog_posts(base_url="http://localhost:5959/blog/")
+    print(latest_blog_posts)
     return templates.TemplateResponse(
-        "layout.html",
+        "social.html",
         context={"request": request, "latest_blog_posts": latest_blog_posts},
     )
 
 
-def get_content(url):
-    response = requests.get(url)
-    webpage_text = "Error"
-    if response.status_code == 200:
-        html = response.content
-        soup = BeautifulSoup(html, "html.parser")
-        webpage_text = soup.get_text()
-    return response, webpage_text
-
-
 @app.post("/{social_media}", response_class=HTMLResponse)
-async def compose(
+async def social_media(
     request: Request, blog_url: Annotated[str, Form()], social_media: str
 ):
     if social_media == "linkedin":
         prompt = compose_linkedin_post
     elif social_media == "twitter":
         prompt = compose_twitter_post
-    response, webpage_text = get_content(blog_url)
+    elif social_media == "tags":
+        prompt = compose_tags
+    elif social_media == "summary":
+        prompt = compose_summary
+    response, post_body = get_post_body(blog_url)
     if response.status_code == 200:
-        response = bot(prompt(webpage_text))
+        response = bot(prompt(post_body))
         text = json.loads(response.content)["post_text"]
     else:
         text = "Error"
     return text
+
+
+# SEARCH-RELATED APIs BELOW
+# ------------------------- #
+
+
+@app.get("/search")
+def search_home(request: Request):
+    return templates.TemplateResponse("search.html", context={"request": request})
+
+
+site_path = Path("site").resolve()
+blog_path = site_path / "blog"
+
+
+from functools import lru_cache
+
+
+@lru_cache
+def get_blog_texts(blog_path) -> dict:
+    # List all index.html files under the blog directory, recursively
+    source_files = [f for f in blog_path.rglob("index.html")]
+
+    sources = {}
+    # print a random source_file
+    for source_file in source_files:
+        soup = BeautifulSoup(source_file.read_text(), "html.parser")
+        post_body = soup.find("div", id="post_body")
+        if post_body is not None:
+            sources[source_file] = post_body.get_text()
+    return sources
+
+
+def find_publication_date(source):
+    publication_date = None
+    for line in source.split("\n"):
+        if "pub_date" in line:
+            publication_date = line.split(":")[1].strip()
+    return publication_date
+
+
+# convert publication_date into URL. Split on "-" and join with "/", and remove "0" from month and day.
+def convert_pubdate_to_url(pubdate):
+    """Convert publication date to URL.
+
+    :param pubdate: Publication date in YYYY-MM-DD format.
+    :return: Publication date in YYYY/MM/DD format with leading zeros removed.
+    """
+
+    if pubdate is None:
+        return ""
+    ymd = pubdate.split("-")
+    ymd[1] = ymd[1].lstrip("0")
+    ymd[2] = ymd[2].lstrip("0")
+    return "/".join(ymd)
+
+
+@app.post("/search/term", response_class=HTMLResponse)
+async def search(search_term: Annotated[str, Form()]):
+    sources = get_blog_texts(blog_path=blog_path)
+    results = []
+    for k, v in sources.items():
+        if search_term in v:
+            results.append(k)
+    if not results:
+        return "<b>No results found.</b>"
+    output = "<ul>"
+    for r in results:
+        pubdate = find_publication_date(sources[r])
+        pubdate_url = convert_pubdate_to_url(pubdate)
+        rel_url = r.relative_to(blog_path)
+        output += f"<li><a href='https://ericmjl.github.io/blog/{pubdate_url}/{rel_url}'>{rel_url}</a></li>"
+    output += "</ul>"
+    return output
