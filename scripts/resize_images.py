e8eb5b1487767277150f91a9df4676432abf8487
Eric Ma <ericmjl@users.noreply.github.com>
2023-12-03 00:05:07 -0500

    feat(blog): Add new blog post 'Embracing Leadership: My Journey at Moderna' (#91)
    * feat(blog): Add new blog post 'Embracing Leadership: My Journey at Moderna'
    
    This commit introduces a new blog post titled 'Embracing Leadership: My Journey at Moderna'.
    The post discusses various aspects of leadership from the perspective of a data science team lead.
    
    * feat(scripts): Update resize_images.py to resize and convert images to WEBP
    
    - Added type hints to function parameters and return types
    - Updated the resize_image function to save images in WEBP format instead of the original format
    - The original image is removed after conversion to WEBP
    - Updated the resize_logos_in_tree function to print a message indicating the image has been converted to WEBP
    - Increased the maximum width for resizing from 600px to 800px
    - Added import statements for os and pathlib.Path modules
    - Noted that the here() function returns a Path object
    
    This update should improve the performance of the website by reducing the size of the images.
    
    * feat(blog): update tags for 'Embracing Leadership: My Journey at Moderna' post
    
    - The tags for the blog post 'Embracing Leadership: My Journey at Moderna' have been significantly expanded to cover a wider range of topics discussed in the post.
    - Removed tags: data science, team lead, technical skills, career growth, career development.
    - Added tags: podcast, hesitation, coding, perfectionism, team, power, environment, technical, credibility, bayesian, network, deep learning, llms, growth, moderna, skiplevels, feedback, psychological safety, google, mistakes, graduation, development, obama, humility, servant.
    - These changes aim to improve the discoverability of the post and make it more relevant to a broader audience.

--- scripts/resize_images.py
+++ scripts/resize_images.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python3
 """Resize images within the repository."""
 
+import os
+from pathlib import Path
 
 from PIL import Image
 from pyprojroot import here
 
 
-def resize_image(image_path, base_width):
+def resize_image(image_path: Path, base_width: int) -> bool:
     with Image.open(image_path) as img:
         if img.size[0] > base_width:
             w_percent = base_width / float(img.size[0])
             h_size = int(float(img.size[1]) * float(w_percent))
             img = img.resize((base_width, h_size), Image.LANCZOS)
-            img.save(image_path)
+            webp_path = image_path.with_suffix(".webp")
+            img.save(webp_path, format="WEBP")
+            os.remove(image_path)
             return True
     return False
 
 
-def resize_logos_in_tree(root_dir, logo_name, max_width):
+def resize_logos_in_tree(root_dir: Path, logo_name: str, max_width: int) -> bool:
     resized_any = False
     for path in root_dir.rglob(logo_name):
         if resize_image(path, max_width):
-            print(f"Resized: {path}")
+            print(f"Resized and converted to WEBP: {path}")
             resized_any = True
     return resized_any
 
 
 if __name__ == "__main__":
-    root_directory = here()
+    root_directory = here()  # This returns a Path object
     logo_filename = "logo.png"
-    maximum_width = 600
+    maximum_width = 800  # Updated to 800px
 
     if resize_logos_in_tree(root_directory, logo_filename, maximum_width):
         print("Some logos were resized. Commit rejected.")
