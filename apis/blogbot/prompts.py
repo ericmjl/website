f23d2f183cc24a36bf91d7714bf4240edcea60ef
Eric Ma <ericmajinglong@gmail.com>
2024-06-10 20:56:26 -0400

    feat(blogbot): add URL parameter to social media post composition functions
    - Updated `compose_linkedin_post`, `compose_patreon_post`, `compose_twitter_post`, and `compose_substack_post` functions to accept a `url` parameter. This allows for the inclusion of the blog post URL directly in the social media posts, using Markdown syntax for linking.
    - Modified the docstrings of the updated functions to reflect the new parameter and its usage in the post composition.
    - Added a new `bannerbot_sysprompt` function to generate banner images for blog posts, focusing on a watercolor style without any text, enhancing the visual appeal of blog posts across platforms.
    
    This update enables more dynamic and engaging social media posts by directly embedding URLs and introduces a creative feature for generating banner images, broadening the functionality of the blogbot API.

--- apis/blogbot/prompts.py
+++ apis/blogbot/prompts.py
@@ -3,19 +3,23 @@ from llamabot.prompt_manager import prompt
 
 
 @prompt
-def compose_linkedin_post(text):
+def compose_linkedin_post(text, url):
     """This is a blog post that I just wrote.
 
         {{ text }}
 
+    It came from the following url: {{ url }}.
+
     Please compose for me a LinkedIn post
     that entices my network on LinkedIn to read it.
-    I usually like to open off with a question that the post answers.
+    Open off with a question that the post answers.
+    Then add an overview of what the post is about but do not reveal too many details.
     Ensure that there is a call to action to interact with the post after reading
     to react with it, comment on it, or share the post with others.
     Include hashtags inline with the LinkedIn post and at the end of the post too.
     Hashtags should be all lowercase.
-    Ensure that there is a placeholder for me to paste in the URL.
+    Ensure that you insert the URL of the blog post in an appropriate place,
+    using Markdown syntax to link to the post.
     Also ensure that it is written in first-person, humble, and inviting tone.
 
     Return in the following json format:
@@ -25,10 +29,12 @@ def compose_linkedin_post(text):
 
 
 @prompt
-def compose_patreon_post(text):
+def compose_patreon_post(text, url):
     """This is a blog post that I just wrote.
 
         {{ text }}
+
+    It came from the following url: {{ url }}.
 
     Based on the blog post, please compose for me a patreon post
     that encourages my Patreon supporters to read it.
@@ -45,10 +51,12 @@ def compose_patreon_post(text):
 
 
 @prompt
-def compose_twitter_post(text):
+def compose_twitter_post(text, url):
     """This is a blog post that I just wrote:
 
         {{ text }}
+
+    It came from the following url: {{ url }}.
 
     Please compose for me a Twitter post
     that entices my followers on Twitter to read it.
@@ -57,7 +65,8 @@ def compose_twitter_post(text):
     such as retweeting, commenting, or sharing it with others.
     Include hashtags inline with the Twitter post.
     Hashtags should be all lowercase.
-    Ensure that there is a placeholder for me to paste in the URL.
+    Ensure that you insert the URL of the blog post in an appropriate place,
+    using Markdown syntax to link to the post.
     Also ensure that it is written in first-person, humble, and inviting tone.
 
     Return in the following json format:
@@ -68,10 +77,12 @@ def compose_twitter_post(text):
 
 
 @prompt
-def compose_substack_post(text):
+def compose_substack_post(text, url):
     """This is a blog post that I just wrote:
 
         {{ text }}
+
+    It came from the following url: {{ url }}.
 
     Please compose for me a Substack post
     that encourages my readers on Substack to read the blog post I just gave you.
@@ -80,8 +91,8 @@ def compose_substack_post(text):
     And then open off with a question that the post answers.
     Ensure that there is a call to action for the reader,
     such as forwarding the post to others they think may benefit from it.
-    Ensure that there is a placeholder for me
-    to paste in the URL to the blog post.
+    Ensure that you insert the URL of the blog post in an appropriate place,
+    using Markdown syntax to link to the post.
     Also ensure that it is written in first-person, humble, and inviting tone.
 
     Return in the following json format:
@@ -110,11 +121,13 @@ def compose_tags(text):
 
 
 @prompt
-def compose_summary(text):
+def compose_summary(text, url):
     """
     Here is my blog post:
 
         {{ text }}
+
+    It came from the following url: {{ url }}.
 
     I need a summary of the post in 100 words or less.
     Please write in first person.
@@ -137,4 +150,30 @@ def fix_json(bad_json):
     Can you fix it for me such that its contents are preserved
     but it is now valid JSON?
     Return only the JSON and nothing else.
+    """
+
+
+@prompt
+def bannerbot_sysprompt():
+    """As 'Banner Artist',
+    your role is to create banner images for blog posts in a watercolor style,
+    with a 16:4 aspect ratio.
+
+    You will be given a chunk of text that comes from the blog post.
+    Use as many concepts from the text as possible to create the banner image.
+
+    Ensure that there is no text in the banner image!
+
+    You will focus on maximizing the use of imagery and symbols to represent ideas,
+    strictly avoiding any text or character symbols.
+
+    Ensure that there is no text in the banner image!
+
+    Your creations should be abstract or symbolic,
+    suitable for a wide range of blog topics.
+    When provided with vague or lacking details in a blog summary,
+    you should make creative assumptions
+    to interpret and visualize the content into an appealing banner image.
+
+    Ensure that there is no text in the banner image!
     """
